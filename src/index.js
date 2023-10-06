2d298265adb4b0b93c632b96a1aa0fe6d246b494
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2023-10-06 10:53:38 +0530

    project 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const page = (
+  <div>
+  <img src="./logo192.png" alt=""></img>
+  <h1>Fun facts about React</h1>
+  <ol>
+    <li>Was first released in 2013</li>
+    <li>was originally created by jordan walke</li>
+    <li>has well over 100k stars on github</li>
+    <li>is maintained by facebook</li>
+    <li>powers thousands of enterprise apps, including mobile apps</li>
+  </ol>
+</div>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>namaste duniya</h1>
+ page
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
